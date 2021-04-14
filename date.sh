19dd39b0603c258a01de92dbecd1d48c007596b9
kiosk619 <82438669+kiosk619@users.noreply.github.com>
2021-04-14 11:39:56 +0200

    Update date.sh

--- date.sh
+++ date.sh
@@ -1,14 +1,5 @@
 /bin/sh -xe
 
-Den=$(date|cut –d „ „ –f1)
+date=$(date)
 
-Case $Den in
-Mon|Tue)
-	Echo Eh, to lazy to start yet;;
-Wed|)
-	Echo oh, yes i am about to start;;
-Thur|Fri)
-	Echo okay, lets do it;;
-*)
-	Echo sorry, but i dont work overtimes;;
-Esac
+echo date
