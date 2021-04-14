46e2f90efe0101e131beb0e5b91b9d2398b1600c
kiosk619 <82438669+kiosk619@users.noreply.github.com>
2021-04-14 11:14:23 +0200

    Update Diskusagecheck.sh

--- Diskusagecheck.sh
+++ Diskusagecheck.sh
@@ -1,5 +1,5 @@
 
-#!/bin/bash
+/bin/sh -xe
 dusage=$(df -Ph | grep -vE '^tmpfs|cdrom' | sed s/%//g | awk '{ if($5 > 60) print $0;}')
 fscount=$(df -Ph | grep -vE '^Filesystem|tmpfs|cdrom' | sed s/%//g | awk '{ if($5 > 60) print $0;}' | wc -l)
 if [ $fscount -ge 2 ]; then
